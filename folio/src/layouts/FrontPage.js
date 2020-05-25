d032c13ef0c9217f63dc37cb8112eb5128948b85
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-25 04:18:16 -0700

    Update FrontPage.js

--- folio/src/layouts/FrontPage.js
+++ folio/src/layouts/FrontPage.js
@@ -135,9 +135,9 @@ function FrontPage() {
           <Divider className={classes.dividerMed} light />
           <br></br>
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={6}>
-                <Card className={classes.card} elevation={0} padding={2} square="true">
+            {cards.map((card,index) => (
+              <Grid item key={index} xs={12} sm={6} md={6}>
+                <Card className={classes.card} elevation={0} padding={2} square={true}>
                 <Link href={card.link}>
                   <CardMedia
                     className={classes.cardMedia}
