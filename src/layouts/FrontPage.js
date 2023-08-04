3e87eb2512ee5e3ab60c5e809b3e0316beb80622
Lauren Johnston <lauren.el.johnston@gmail.com>
2023-08-03 23:59:48 -0700

    remove unused import

--- src/layouts/FrontPage.js
+++ src/layouts/FrontPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
